82d8b58055a53df64f0dc155f7f31e33c32b62bb
Pedro <pedropm.lolo05@gmail.com>
2026-04-01 11:22:00 +0000

    connect mongodb

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
-
 app.use(express.json())
+
+// TODO: mover esto a .env !!!
+mongoose.connect('process.env.MONGODB_URI')
+  .then(() => console.log('conectado a mongo'))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.json({ message: 'BildyApp API' })
