8b72d7ea7f10419382475fcc2e8cafbe86c9165d
Pedro <pedropm.lolo05@gmail.com>
2026-04-01 11:35:00 +0000

    fix: move mongo uri to env, never hardcode credentials!!

--- src/index.js
+++ src/index.js
@@ -4,8 +4,13 @@ import mongoose from 'mongoose'
 const app = express()
 app.use(express.json())
 
-// TODO: mover esto a .env !!!
-mongoose.connect('process.env.MONGODB_URI')
+const MONGO_URI = process.env.MONGODB_URI
+if (!MONGO_URI) {
+  console.error('falta MONGODB_URI en .env')
+  process.exit(1)
+}
+
+mongoose.connect(MONGO_URI)
   .then(() => console.log('conectado a mongo'))
   .catch(err => console.log(err))
 
@@ -13,6 +18,6 @@ app.get('/', (req, res) => {
   res.json({ message: 'BildyApp API' })
 })
 
-app.listen(3000, () => {
-  console.log('servidor corriendo en 3000')
+app.listen(process.env.PORT || 3000, () => {
+  console.log('servidor corriendo')
 })
