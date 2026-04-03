714b3ab592a1adc91d58ed66d75c022fb103f07b
Pedro <pedropm.lolo05@gmail.com>
2026-04-03 14:40:00 +0000

    añadir modelo Company

---
+++ src/models/Company.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const companySchema = new mongoose.Schema({
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  name: { type: String, required: true },
+  cif:  { type: String, required: true, unique: true },
+  address: {
+    street: String, number: String,
+    postal: String, city: String, province: String
+  },
+  logo: String,
+  isFreelance: { type: Boolean, default: false }
+}, { timestamps: true })
+
+export const Company = mongoose.model('Company', companySchema)
