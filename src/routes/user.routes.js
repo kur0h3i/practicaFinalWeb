61d6cadf24a67d0d88cd5e3a3ba403ce70f9b6bd
Pedro <pedropm.lolo05@gmail.com>
2026-04-04 15:00:00 +0000

    definir todas las rutas de usuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,32 @@
 import { Router } from 'express'
-import { register, login } from '../controllers/user.controller.js'
+import { protect } from '../middleware/auth.middleware.js'
+import { validate } from '../middleware/validate.js'
+import {
+  registerSchema, verificationSchema, loginSchema,
+  personalDataSchema, companySchema, changePasswordSchema, inviteSchema
+} from '../validators/user.validator.js'
+import {
+  register, verifyEmail, login, updatePersonalData,
+  updateCompany, uploadCompanyLogo, getMe,
+  refreshToken, logout, deleteUser, changePassword, inviteUser
+} from '../controllers/user.controller.js'
 
 const router = Router()
 
-router.post('/register', register)
-router.post('/login', login)
+router.post('/register',    validate(registerSchema), register)
+router.post('/login',       validate(loginSchema), login)
+router.post('/refresh',     refreshToken)
+
+router.use(protect)
+
+router.put('/validation',   validate(verificationSchema), verifyEmail)
+router.put('/register',     validate(personalDataSchema), updatePersonalData)
+router.patch('/company',    validate(companySchema), updateCompany)
+router.patch('/logo',       uploadCompanyLogo)   // TODO: falta multer aqui
+router.get('/',             getMe)
+router.post('/logout',      logout)
+router.delete('/',          deleteUser)
+router.put('/password',     validate(changePasswordSchema), changePassword)
+router.post('/invite',      validate(inviteSchema), inviteUser) // TODO: falta comprobar rol admin
 
 export default router
